bd91ff3a3932ed6504e3cd899d31417bc2937016
manjeet <manjeet7171gautam@gmail.com>
2024-06-29 11:41:52 +0530

    hover img box

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
+
+var fixed = document.querySelector('#fixed-image')
+var elem1 = document.querySelector('#elem1')
+var elemC = document.querySelector('#elem-container')
+
+var elems = document.querySelectorAll(".elem")
+
+
+elemC.addEventListener("mouseenter", function(){
+    fixed.style.display="block"
+})
+elemC.addEventListener("mouseleave", function(){
+    fixed.style.display="none"
+})
+
+
+elems.forEach((e)=>{
+        e.addEventListener("mouseenter", function(){
+            var image = e.getAttribute('data-image')
+            fixed.style.backgroundImage=`url(${image})`
+        })
+        
+})
+
